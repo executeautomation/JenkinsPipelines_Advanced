1a15b51b90a0af5c6df3d3fe5923227c547d6790
ExecuteAutomation <kartmcad@gmail.com>
2020-05-30 16:59:35 +1200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,11 @@
-Write-Output "Hello all from file"
+ try
+                {
+                    $response = Invoke-WebRequest -Uri http://localhost:81/ -ErrorAction Stop
+                    # This will only execute if the Invoke-WebRequest is successful.
+                    $StatusCode = $Response.StatusCode
+                }
+                catch
+                {
+                    $StatusCode = $_.Exception.Response.StatusCode.value__
+                }
+                $StatusCode
